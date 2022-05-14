8f4374c91a8c609fdbbdaeb7084e8d1e5ac49135
KanishkVijaywargiya <secretcodersociety@gmail.com>
2022-05-14 12:59:21 +0530

    Initial Commit

---
+++ Canvas Editor/Canvas_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Canvas_EditorApp.swift
+//  Canvas Editor
+//
+//  Created by Kanishk Vijaywargiya on 14/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Canvas_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Canvas Editor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Canvas Editor
+//
+//  Created by Kanishk Vijaywargiya on 14/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
